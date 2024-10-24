c4b7d10900292d9cc40f91dd7c1732b6cbff4eb4
TomasVil12 <tomasxvilla@gmail.com>
2024-10-24 17:41:29 -0500

    inicio ejercicios agregacion

---
+++ Biblioteca/src/com/biblioteca/mantener/General.java
@@ -0,0 +1,37 @@
+public class General {
+    private int codigo;
+    private String nombre;
+
+    public General(){
+
+    }
+
+    public General(int codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public boolean validarDatos() {
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "General { Codigo: " + codigo + " Nombre: " + nombre + '}';
+    }
+}
---
+++ Biblioteca/src/com/biblioteca/mantener/Prestatario.java
@@ -0,0 +1,51 @@
+
+public class Prestatario extends General{
+    private String direccion;
+    private String telefono;
+    private String ruc;
+    private Ciudad ciudad;
+
+    public Prestatario() {
+
+    }
+
+    public Prestatario(int codigo, String nombre, String direccion, String telefono, String ruc, Ciudad ciudad) {
+        super(codigo, nombre);
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.ruc = ruc;
+        this.ciudad = ciudad;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+    public String getRuc() {
+        return ruc;
+    }
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+    @Override
+    public String toString() {
+        return "Prestatario {" + super.toString() + " Direccion: " + direccion + " Telefono: " + telefono + " ruc: " + ruc  + " Ciudad: " + ciudad + '}';
+    }
+
+
+}
